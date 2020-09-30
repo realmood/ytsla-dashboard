0ebf57e8d6e3355833214c58e3466413f8a84e9f
realmood <realmood@users.noreply.github.com>
2020-09-30 14:38:27 +0200

    Add correct uniswap link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
           <p className='btn-text'>yTSLA is an experimental Parody token and has no affiliation with TESLA motors nor Elon Musk, nor MEME.   yTSLA was given away for free as a novelty airdrop and farm yield.    All original holders of yTSLA received it in a decentralized manner for no cost.    The inherent value of yTSLA is 0 and if it seems to be trading on the open market for more, be very aware that the value can return again to zero very quickly.  Only use this as a parody,  please do not take it seriously.</p>
         </div>
         <Button className='footer-item btn-text' href={`${Config.etherscan}${Config.Token.address}`} target='_blank' >yTSLA Contract</Button>
-        <Button className='footer-item btn-text' href='https://app.uniswap.org' target='_blank' >Uniswap YTSLA-ETH</Button>
+        <Button className='footer-item btn-text' href='https://app.uniswap.org/#/swap?outputCurrency=0x5322A3556F979cE2180B30e689a9436fDDCB1021' target='_blank' >Uniswap YTSLA-ETH</Button>
         <Button className='footer-item btn-text' href='https://twitter.com/yTSLAFi' target='_blank' >Twitter</Button>
         <Button className='footer-item btn-text' href='https://t.me/yTSLA_lounge' target='_blank' >Telegram</Button>
       </div>
